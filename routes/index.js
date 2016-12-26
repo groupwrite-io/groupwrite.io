fb6a2f3940186423d1388dd675a66616186a8389
Ron Gross <chiefninjaofficer@gmail.com>
2016-12-27 01:50:05 +0200

    Switch view engine to mustache

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'writing.io' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'writing.io'
+    });
 });
 
 module.exports = router;
