e89b12daf09819201feb26c1fb4b85d2a9c0a8f5
Ron Gross <chiefninjaofficer@gmail.com>
2016-12-31 03:29:36 +0200

    Delete everything

--- client/js/app.js
+++
@@ -1,5 +0,0 @@
-console.log("Welcome to writing.io");
-
-var module = require('../modules/myModule');
-console.log(module.getGreeting('Nader'));
-// asdf
--- test/test.js
+++
@@ -1,6 +0,0 @@
-var should = require('should');
-describe('my first test', function () {
-    it('is running', function () {
-        true.should.equal(true);
-    });
-});
