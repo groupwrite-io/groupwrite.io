d010e9a73e80c049386db3604454b63e6c8579d2
Ron Gross <chiefninjaofficer@gmail.com>
2017-01-06 19:25:48 +0200

    Read nickname

--- public/client.js
+++ public/client.js
@@ -1,6 +1,13 @@
 var app = new Vue({
     el: '#app',
     data: {
-        title: 'write.io'
+        title: 'write.io',
+        nickname: ''
+    },
+
+    methods: {
+        login: function () {
+            alert("Hello " + this.nickname);
+        }
     }
 })
