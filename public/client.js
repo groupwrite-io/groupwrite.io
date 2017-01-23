0ba92fafe5722e6b189f324db873a70f1a433968
Ron Gross <chiefninjaofficer@gmail.com>
2017-01-23 22:21:33 +0200

    Some progress

--- public/client.js
+++ public/client.js
@@ -17,3 +17,18 @@ socket.on('server:state', function (state) {
 });
 
 var myNickname = '';
+
+
+var Vue = require('vue').Vue;
+    var Home = require('./Home.vue').Home;
+    var Game = require('./Game.vue').Game;
+    /*
+    import Vue from 'vue';
+    import Home from './Home.vue';
+    import Game from './Game.vue';
+    */
+
+    new Vue({
+        el: '#app',
+        render: h => h(Home)
+    })
