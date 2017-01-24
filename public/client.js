a745ec7a26ba7f5e03d4d7d367c6f693d12331ec
Ron Gross <chiefninjaofficer@gmail.com>
2017-01-24 11:17:31 +0200

    Remove jquery

--- public/client.js
+++ public/client.js
@@ -1,17 +1,4 @@
 var socket = require('socket.io-client')();
-var $ = require('jQuery');
-
-$.postJSON = function (url, data, callback) {
-    return jQuery.ajax({
-        'type': 'POST',
-        'url': url,
-        'contentType': 'application/json',
-        'data': JSON.stringify(data),
-        'dataType': 'json',
-        'success': callback
-    });
-};
-
 socket.on('server:state', function (state) {
     pages.game.players = state.players;
 });
