8045ec0b93db7ff9df06d0f815aae8831f2f8070
Ron Gross <chiefninjaofficer@gmail.com>
2017-01-25 16:10:36 +0200

    Add home html

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,12 @@
 <template>
-    <div>home</div>
+    <div>
+        <div class="center">
+            <h1>write.io</h1>
+
+            <p>Pick a username</p>
+            <form onsubmit="return false">
+<input id="choosenickname" placeholder="Anonymous" v-model="nickname" autofocus/>
+<button id="write-btn" v-on:click="login">Write!</button>
+</form>
+</div>
 </template>
