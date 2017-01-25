1c0d5da459ac9ad5995d9bec1c140c04592c9c1d
Ron Gross <chiefninjaofficer@gmail.com>
2017-01-25 15:54:50 +0200

    Add routes /foo /bar

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,25 @@
   <div id="app">
     <img src="./assets/logo.png">
     <hello></hello>
+
+    <p>
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
+    </p>
+  <router-view></router-view>
+
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+  import Hello from './components/Hello'
 
-export default {
-  name: 'app',
-  components: {
-    Hello
+  export default {
+    name: 'app',
+    components: {
+      Hello
+    }
   }
-}
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,36 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+// 1. Define route components.
+// These can be imported from other files
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// Vue.extend(), or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for routes: routes
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
+  router,
   components: { App }
 })
