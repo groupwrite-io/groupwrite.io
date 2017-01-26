d9d610d6b64ea077dea6e7d399b9efe493b2c458
Ron Gross <chiefninjaofficer@gmail.com>
2017-01-26 19:56:53 +0200

    e2e tests pass

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -2,20 +2,20 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': function (browser) {
-    // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
-    const devServer = browser.globals.devServerURL
+    'default e2e tests': function (browser) {
+        // automatically uses dev Server port from /config.index.js
+        // default: http://localhost:8080
+        // see nightwatch.conf.js
+        const devServer = browser.globals.devServerURL
 
-    browser
-      .url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
-      .end()
-  }
+        browser
+            .url(devServer)
+            .waitForElementVisible('#app', 5000)
+            // .assert.elementPresent('.')
+            .assert.containsText('.home h1', 'write.io')
+            // .assert.elementCount('img', 1)
+            .end()
+    }
 }
 
 
