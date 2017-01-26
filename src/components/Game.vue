f79a404f24fd9d05ff50e0a7da5d7b2c6d3b4c41
Ron Gross <chiefninjaofficer@gmail.com>
2017-01-26 10:35:50 +0200

    Add game vue script

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -9,3 +9,36 @@
     <button v-on:click="quit">Quit</button>
   </div>
 </template>
+
+<script>
+  import Home from './home.vue'
+
+  export default {
+    name: 'game',
+
+    delimiters: ['${', '}'],
+    data() {
+      return {
+        seen: false,
+        players: [],
+        nickname: null
+      }
+    },
+    methods: {
+      quit: function () {
+        var request = require('superagent')
+        request.post('/api/quit', {
+          nickname: this.nickname
+        }, function (state) {
+          // TODO handle quit error
+        })
+        this.seen = false
+        Home.seen = true
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
