e3ae114ecb1017ac8e0111fd720d713a48f94676
Ron Gross <chiefninjaofficer@gmail.com>
2017-01-28 11:38:05 +0200

    Adding Queue vue

---
+++ src/components/Queue.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='waiting'>
+    <h1>Waiting for players {{sharedState.players.length}}/3</h1>
+    <h2>We'll play a gong when the game start (turn up your spakers)</h2>
+    <button id='quit-btn' v-on:click="quit">Quit</button>
+  </div>
+</template>
+
+<script>
+  import assert from 'assert'
+  import store from './store'
+  // import PlayerList from './PlayerList.vue'
+
+  var VueRouter = require('vue-router')
+  var router = new VueRouter()
+
+  export default {
+    name: 'Queue',
+    components: {
+      PlayerList
+    },
+    data() {
+      return {
+        sharedState: store.state
+      }
+    }
+  }
+</script>
