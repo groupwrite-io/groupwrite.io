410f14264a76c5f15034f3e04d60030fe1ec8c5f
DorWeid <dorprozor@gmail.com>
2017-02-15 20:29:50 +0200

    Initial server api config

---
+++ config/server.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MAX_PLAYERS_IN_GAME: 3,
+    noPlayerFoundMessage: id => `No player found with ID ${id}`,
+    adminKey: 'nalkFaoKsjd78', // TODO: This should be encrypted
+};
