636c4f339b69c0bd7a3be32ca7337eed924fd221
MenyChiko <tziko770@gmail.com>
2017-02-15 21:55:54 +0200

    Basic player list style changes

--- src/components/PlayerList.vue
+++ src/components/PlayerList.vue
@@ -2,81 +2,81 @@
   <div class="playerList">
     <h2>Players</h2>
     <ol class="nonelist">
-      <li class="playerListItem" v-for="player in sharedState.players">
-        <div v-if="player.nickname != sharedState.myNickname" class="playerbox">
-          <div class="nickname">
-            {{player.nickname}}:
+      <li class="playerListItem form-control" v-for="player in sharedState.players"  
+          v-if="player.nickname != sharedState.myNickname">
+          <div class="row playerRow">
+            <div class="nickname col-md-4">
+              {{player.nickname}}:
+            </div>
+            <div class="suggestion col-md-6">
+              {{player.suggestion}}
+            </div>
+            <div class="vote-button col-md-2">
+            </div>
           </div>
-          <div class="suggestion">
-            {{player.suggestion}}
-          </div>
-          <div class="vote-button">
-          </div>
-        </div>
       </li>
     </ol>
   </div>
 </template>
 <script>
-  import store from './store'
+    import store from './store'
 
-  export default {
-    name: 'PlayerList',
-    props: ['players'],
-    data() {
-      return {
-        sharedState: store.state
-      }
+    export default {
+        name: 'PlayerList',
+        props: ['players'],
+        data() {
+            return {
+                sharedState: store.state
+            }
+        }
     }
-  }
-
 </script>
 <style>
-
-  body {
-      margin: 0;
-      padding: 0;
-  }
-
-  ol.nonelist {
-    list-style-type: none;
-    position: absolute;
-    left: 0px;
+    body {
+        margin: 0;
+        padding: 0;
     }
-
-  .nickname {
-    display: inline-block;
-    text-align: center;
-    width: 100px;
-    height: 50px;
-    background: #f5f5f0;
-  }
-
-  .suggestion {
-    display: inline-block;
-    width: 300px;
-    height: 50px;
-    text-align: left;
-    word-wrap: break-word;
-    border: 2px solid gray;
-
-  }
-
-  .vote-button {
-    display: inline-block;
-    background-image: url('../assets/heart-icon.png');
-    width: 32px;
-    height: 32px;
-    cursor: pointer;
-    position: absolute;
-  }
-
-  .vote-button:hover {
-    background-image: url('../assets/heart-icon-hover.png');
-  }
-
-  .displayblock {
-      display: block;
+    
+    ol.nonelist {
+        list-style-type: none;
     }
-
+    
+    .playerListItem {
+        background-color: #f5f5f0;
+    }
+    
+    .nickname {
+        display: inline-block;
+        text-align: center;
+        width: 100px;
+        height: 50px;
+        padding: 15px 0;
+    }
+    
+    .suggestion {
+        display: inline-block;
+        width: 300px;
+        height: 50px;
+        text-align: left;
+        word-wrap: break-word;
+        border: 2px solid gray;
+        background-color: #FFFFFF;
+    }
+    
+    .vote-button {
+        display: inline-block;
+        background-image: url('../assets/heart-icon.png');
+        background-repeat: no-repeat;
+        width: 32px;
+        height: 32px;
+        cursor: pointer;
+    }
+    
+    .vote-button:hover {
+        background-image: url('../assets/heart-icon-hover.png');
+    }
+    
+    .displayblock {
+        display: block;
+    }
 </style>
