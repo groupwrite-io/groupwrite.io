2656814320c10a36f06f9f31dbe6e19fbb035e71
Ron Gross <chiefninjaofficer@gmail.com>
2017-02-19 17:05:39 +0200

    Default to dev mode

--- config/secret.config.js
+++ config/secret.config.js
@@ -1,16 +1,14 @@
 const mode = process.env.mode
 
 switch (mode) {
-  case 'dev':
-    module.exports = require('./dev.secret.config')
-    break;
-
   case 'prod':
     module.exports = require('./prod.secret.config')
     break;
 
+  case 'dev':
   default:
-    throw Error(`Unknown mode: ${mode}`)
+    module.exports = require('./dev.secret.config')
+    break;
 }
 
 console.log(`Secret config loaded for mode '${mode}'`)
