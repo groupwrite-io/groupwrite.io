9f62106b4efa44f45026cfa8c7da5c2487dd7b41
Ron Gross <chiefninjaofficer@gmail.com>
2017-02-20 14:31:35 +0200

    Add API to get one story

--- api/stories.js
+++ api/stories.js
@@ -7,4 +7,11 @@ module.exports = function (router) {
       res.status(200).send(stories)
     })
   })
+
+  router.get('/stories/get', function (req, res, next) {
+    let storyId = req.query.storyId
+    Story.findOne({ _id: storyId }, (err, story) => {
+      res.status(200).send(story)
+    })
+  })
 }
