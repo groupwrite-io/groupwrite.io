cab44bc6507336555d856cae9ac5e9327c44c7f8
Ron Gross <chiefninjaofficer@gmail.com>
2017-03-23 12:08:51 +0200

    Minor

--- src/components/GameOverPage.vue
+++ src/components/GameOverPage.vue
@@ -1,34 +1,35 @@
 <template>
   <div class="game">
-
+  
     <div class='container'>
       <h1>Game Over</h1>
       <div class='row'>
       </div>
       <story></story>
-      <p>In order to find your story in the future, just <a :href="'/#/story?id=' + sharedState.story.id" target="_blank">click here.</a></p>
+      <p>In order to find your story in the future, just <a :href="'/#/story?id=' + sharedState.story.id"
+           target="_blank">click here.</a></p>
     </div>
   </div>
 </template>
 <script>
-  import store from './store'
-  import Story from './Story'
+import store from './store'
+import Story from './Story'
 
-  //  var request = require('superagent')
+//  var request = require('superagent')
 
-  export default {
-    name: 'GameOverPage',
-    components: {
-      Story
-    },
-    data() {
-      return {
-        sharedState: store.state
-      }
-    },
-    methods: {
+export default {
+  name: 'GameOverPage',
+  components: {
+    Story
+  },
+  data() {
+    return {
+      sharedState: store.state
     }
+  },
+  methods: {
   }
+}
 
 </script>
 <style>
